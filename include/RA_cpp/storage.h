e101df65277fecc7aad24fa50c15d523a058bb9a
Stuart Croy <stuartcroy@mac.com>
2023-08-27 01:59:57 +0100

    storage.h/cols_to_stream - Bug fix for 0 columns case.

--- include/RA_cpp/storage.h
+++ include/RA_cpp/storage.h
@@ -291,6 +291,9 @@ struct IStorage
 // might want to share pool across the whole relation, or even across relations
 // => need pool to use shared pointers
 
+// FIXME: use std::pmr::string and allow separate allocators for strings
+// (and perhaps per column?)
+
 template<typename T>
 struct column_storage_base
 {
@@ -774,65 +777,66 @@ std::ostream& cols_to_stream(
 )
 {
     const size_t n_cols = cols.size();
-    const size_t n_rows = cols[ 0 ]->size();
+    if (n_cols > 0) {
+        const size_t n_rows = cols[ 0 ]->size();
 
-    // Work out column widths
-    std::vector<size_t> col_sizes( n_cols );
-    for ( size_t c = 0; c < n_cols; ++c )
-    {
-        col_sizes[ c ] = col_tys[ c ].first.size();
-    }
-
-    std::ostringstream ss;
-    size_t total_width = 0;
-    for ( size_t c = 0; c < n_cols; ++c )
-    {
-        size_t m = col_sizes[ c ];
-        for (auto it = cols[ c ]->cbegin(); it != cols[ c ]->cend(); ++it )
+        // Work out column widths
+        std::vector<size_t> col_sizes( n_cols );
+        for ( size_t c = 0; c < n_cols; ++c )
         {
-            ss.str("");
-            ops[ c ]->to_stream( it.get(), ss );
-            m = std::max( size_t( ss.tellp() ), m );
+            col_sizes[ c ] = col_tys[ c ].first.size();
         }
-        col_sizes[ c ] = m;
 
-        if (c > 0) {
-            os << "  ";
-            total_width += 2;
-        }
-        ss.str("");
-        ss.width( static_cast<long>(m) );
-        ss << col_tys[ c ].first;
-        ss.width( 0 );
-        os << ss.str();
-        total_width += m;
-    }
-    os << "\n";
-
-    for ( size_t i=0; i < total_width; ++i )
-    {
-        os << "-";
-    }
-    os << "\n";
-
-    // values
-
-    for ( size_t r = 0; r < n_rows; ++r )
-    {
-        for( size_t c = 0; c < n_cols; ++c )
+        std::ostringstream ss;
+        size_t total_width = 0;
+        for ( size_t c = 0; c < n_cols; ++c )
         {
+            size_t m = col_sizes[ c ];
+            for (auto it = cols[ c ]->cbegin(); it != cols[ c ]->cend(); ++it )
+            {
+                ss.str("");
+                ops[ c ]->to_stream( it.get(), ss );
+                m = std::max( size_t( ss.tellp() ), m );
+            }
+            col_sizes[ c ] = m;
+
             if (c > 0) {
                 os << "  ";
+                total_width += 2;
             }
             ss.str("");
-            ss.width( static_cast<long>(col_sizes[ c ]) );
-            ops[ c ]->to_stream( cols[ c ]->at( r ), ss );
-            ss.width(0);
+            ss.width( static_cast<long>(m) );
+            ss << col_tys[ c ].first;
+            ss.width( 0 );
             os << ss.str();
+            total_width += m;
         }
         os << "\n";
-    }
 
+        for ( size_t i=0; i < total_width; ++i )
+        {
+            os << "-";
+        }
+        os << "\n";
+
+        // values
+
+        for ( size_t r = 0; r < n_rows; ++r )
+        {
+            for( size_t c = 0; c < n_cols; ++c )
+            {
+                if (c > 0) {
+                    os << "  ";
+                }
+                ss.str("");
+                ss.width( static_cast<long>(col_sizes[ c ]) );
+                ops[ c ]->to_stream( cols[ c ]->at( r ), ss );
+                ss.width(0);
+                os << ss.str();
+            }
+            os << "\n";
+        }
+    }
     return os;
 }
 
